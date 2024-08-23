a36b806bfe7d8f870dd67cf1cc78aa028bebdcbb
cm <redox-rust@hotmail.com>
2024-08-23 16:16:50 +0800

    featrue: using opentelemetry-otlp for tracing

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Eco system
+
+## tag v4-3-open-telemetry
+```Shell
+> sudo docker run -d -p 16686:16686 -p 4317:4317 -e COLLECTOR_OTLP_ENABLED=true jaegertracing/all-in-one:latest 
+
+> cargo run --example axum_tracing 
+```
--- examples/axum_tracing.rs
+++ examples/axum_tracing.rs
@@ -1,13 +1,19 @@
-use std::time::Duration;
-
 use anyhow::Result;
-use axum::{routing::get, Router};
+use axum::{extract::Request, routing::get, Router};
+use opentelemetry::{trace::TracerProvider, KeyValue};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+  runtime,
+  trace::{Config, RandomIdGenerator, Tracer},
+  Resource,
+};
+use std::time::Duration;
 use tokio::{
+  join,
   net::TcpListener,
   time::{sleep, Instant},
 };
-use tracing::{debug, info, level_filters::LevelFilter, warn};
-
+use tracing::{debug, info, instrument, level_filters::LevelFilter, warn};
 use tracing_subscriber::{
   fmt::{self, format::FmtSpan},
   layer::SubscriberExt,
@@ -17,14 +23,20 @@ use tracing_subscriber::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
+  // console layer for tracing-subscriber
   let console =
     fmt::Layer::new().with_span_events(FmtSpan::CLOSE).pretty().with_filter(LevelFilter::DEBUG);
 
+  // file layer for tracing-subscriber
   let file_appender = tracing_appender::rolling::daily("/tmp/logs", "ecosystem.log");
   let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-  let file = fmt::Layer::new().with_writer(non_blocking).pretty().with_filter(LevelFilter::DEBUG);
+  let file = fmt::Layer::new().with_writer(non_blocking).pretty().with_filter(LevelFilter::INFO);
 
-  tracing_subscriber::registry().with(console).with(file).init();
+  // opentelemetry layer for tracing-subscriber
+  let tracer = init_tracer()?;
+  let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+  tracing_subscriber::registry().with(console).with(file).with(opentelemetry).init();
 
   let addr = "0.0.0.0:8080";
   let app = Router::new().route("/", get(index_handler));
@@ -34,7 +46,8 @@ async fn main() -> Result<()> {
   Ok(())
 }
 
-async fn index_handler() -> &'static str {
+#[instrument(fields(http.uri = req.uri().path(),http.method = req.method().as_str()))]
+async fn index_handler(req: Request) -> &'static str {
   debug!("index handler started");
   sleep(Duration::from_millis(10)).await;
   let ret = long_task().await;
@@ -42,10 +55,50 @@ async fn index_handler() -> &'static str {
   ret
 }
 
+#[instrument]
 async fn long_task() -> &'static str {
   let start = Instant::now();
-  sleep(Duration::from_millis(112)).await;
+  let s1 = sleep(Duration::from_millis(10));
+
+  let t1 = task1();
+  let t2 = task2();
+  let t3 = task3();
+  join!(s1, t1, t2, t3);
+
   let elapsed = start.elapsed().as_millis() as u64;
   warn!(app.task_duration = elapsed, "task tasks too long");
   "Hello, World!"
+}
+
+#[instrument]
+async fn task1() {
+  sleep(Duration::from_millis(15)).await;
+}
+
+#[instrument]
+async fn task2() {
+  sleep(Duration::from_millis(30)).await;
+}
+
+#[instrument]
+async fn task3() {
+  sleep(Duration::from_millis(60)).await;
+}
+
+fn init_tracer() -> Result<Tracer> {
+  let otlp_exporter =
+    opentelemetry_otlp::new_exporter().tonic().with_endpoint("http://localhost:4317");
+  let tracer_provider = opentelemetry_otlp::new_pipeline()
+    .tracing()
+    .with_exporter(otlp_exporter)
+    .with_trace_config(
+      Config::default()
+        .with_id_generator(RandomIdGenerator::default())
+        .with_max_events_per_span(32)
+        .with_max_attributes_per_span(64)
+        .with_resource(Resource::new(vec![KeyValue::new("service.name", "axum-tracing")])),
+    )
+    .install_batch(runtime::Tokio)?;
+  let tracer = tracer_provider.tracer("axum-tracing");
+  Ok(tracer)
 }
